8ff9e842412b31678fce2af4c80ed2b2bf99df3d
Daniel <daniel.ferreira@eduzz.com>
2018-08-06 14:09:00 -0300

    modifica botão de seleção de história

--- src/components/SelectStory/index.js
+++ src/components/SelectStory/index.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 // import './App.css';
 
  const style = require('./selectStory.css');
  console.log('ae',style);
 
 class SelectStory extends Component {
+
+  constructor(){
+    super();
+    this.state = {selected: 0}
+  }
   
   componentDidMount(){
     //this.props.selectStory(1);
+  }
+
+  selected(id){
+    console.log(id);
+    this.setState({selected: id});
+    this.props.selectStory(id);
   }
   
   render() {
@@ -19,19 +31,22 @@ class SelectStory extends Component {
     return (
       <div className={style.component}>
         <div className='container'>
-          {console.log(this.props)}
+        <div className={'stories'}>
           {this.props.stories.map((story, index) => {
             const img = require(`../../${story.img}`);
             return(
-              <div onClick={() => this.props.selectStory(story.id)} key={index} className='story' style={{cursor: 'pointer'}}>
+              <div onClick={() => this.selected(story.id)} 
+              key={index} 
+              className={`story ${this.state.selected == story.id ? 'selected' : ''}`}>
                 <h1>{story.title}</h1>
                 {/* <div><img alt='crianças tirando sarro de uma criança' src={img} /></div> */}
                 <p>{story.description}</p>
               </div>
             );
           })}
+          </div>
+        <Link className='btn-select' to={'/'}><Button variant="contained" color="primary">selecionar</Button></Link>
         </div>
-        <Link to={'/'}><button>selecionar</button></Link>
       </div>
     );
   }
